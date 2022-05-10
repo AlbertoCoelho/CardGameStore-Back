9a1ba0f55c7ecd7cd3f7319e839fa2d7887ed6cb
Alberto <tejotaesi@gmail.com>
2022-05-10 09:39:14 -0300

    prepare to deploy

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import db from "../db.js";
+
+export async function login(req, res) {
+  const { email,password } = req.body;
+
+  try {
+    const user = await db.collection("users").findOne({ email });
+    if(!user) return res.sendStatus(401);
+    if(user && bcrypt.compareSync(password, user.password)) {
+      const token = uuid();
+      await db.collection("sessions").insertOne({token, userId: user._id});
+      res.send(token);
+      return;
+    } 
+
+    res.sendStatus(401); // Unauthorized
+
+  } catch (err) {
+    console.log("Error logging in user.", err);
+    res.status(500).send("Error logging in user.");
+  }
+}
