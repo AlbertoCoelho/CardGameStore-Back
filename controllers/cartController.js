6fae99298bae9a55f5a7cd3290ed9c55fce01cd7
acfernanda <acfernanda98@gmail.com>
2022-05-14 10:49:20 -0300

    get cart products

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,48 +1,59 @@
 import db from "../db.js";
+import { ObjectId } from "mongodb";
 
 export async function addProduct(req, res) {
-  const { user } = req.locals;
+  const { user } = res.locals;
   const { productId } = req.body;
 
   try {
-    const product = await db
-      .collection("products")
-      .findOne({ _id: productId })
-      .toArray();
-    await db
-      .collection("cart")
-      .insertOne({ productId: product[0]._id, user: user._id });
-    res.sendStatus(201);
-  } catch (e) {
-    console.log(e);
+    const cartCollection = db.collection("cart");
+    let userCart = await cartCollection.findOne({ userId: user._id });
+
+    if (!userCart) {
+      await cartCollection.insertOne({ userId: user._id, products: [] });
+      userCart = await cartCollection.findOne({ userId: user._id });
+    }
+
+    const allProducts = db.collection("products");
+    const product = await allProducts.findOne({ _id: new ObjectId(productId) });
+
+    await cartCollection.updateOne(
+      { _id: userCart._id },
+      { $push: { products: product } }
+    );
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
     res.sendStatus(500);
   }
 }
 
-export async function getProducts(req, res) {
-  const { user } = req.locals;
+export async function getCartProducts(req, res) {
+  const { user } = res.locals;
 
   try {
-    const products = await db
-      .collection("products")
-      .findMany({ user: user._id })
-      .toArray();
-    res.send(products);
+    const cartCollection = db.collection("cart");
+    const userCart = await cartCollection.findOne({ userId: user._id });
+
+    if (!userCart) {
+      return res.sendStatus(404);
+    }
+    res.send(userCart);
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
   }
 }
 
+// NÃO ESTÁ CERTA
 export async function deleteProduct(req, res) {
-  const { user } = req.locals;
+  const { user } = res.locals;
   const productId = req.body; // {_id}
 
   try {
     const product = await db
       .collection("cart")
-      .findOne({ productId: productId, user: user._id })
-      .toArray();
+      .findOne({ products: { _id: productId } });
     await db.collection("cart").deleteOne({ ...product });
     res.sendStatus(201);
   } catch (e) {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,20 @@
 import db from "../db.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 export async function signUp(req, res) {
   const user = req.body;
-   
+
   try {
     const SALT = 10;
     const passwordHash = bcrypt.hashSync(user.password, SALT);
     delete user.confirmPassword;
 
-    await db.collection('users').insertOne({ ...user, password: passwordHash, products: [] }); 
+    const users = await db
+      .collection("users")
+      .insertOne({ ...user, password: passwordHash });
+
+    const cartCollection = db.collection("cart");
+    await cartCollection.insertOne({ userId: users.insertedId, products: [] });
     res.sendStatus(201);
   } catch (e) {
     console.log(e);
